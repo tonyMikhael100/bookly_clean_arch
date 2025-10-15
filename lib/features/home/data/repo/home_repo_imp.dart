691832ce5eed96d7e60c638066a44e8731358864
Tony <tonymikhael100@gmail.com>
2025-10-15 10:33:55 +0300

    feat: Implement HomeRepoImp for managing book data retrieval from local and remote sources

---
+++ lib/features/home/data/repo/home_repo_imp.dart
@@ -0,0 +1,43 @@
+import 'package:bookly_clean_arch/core/errors/failure.dart';
+import 'package:bookly_clean_arch/features/home/data/data_source/home_local_data_source.dart';
+import 'package:bookly_clean_arch/features/home/data/data_source/home_remote_data_source.dart';
+import 'package:bookly_clean_arch/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_clean_arch/features/home/domain/repo/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImp implements HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImp(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      //check if there is an cached books
+      var cachedBooks = homeLocalDataSource.fetchNewestBooks();
+      if (cachedBooks.isNotEmpty) {
+        return right(cachedBooks);
+      }
+      var books = await homeRemoteDataSource.fetchNewestBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure(error: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookEntity>>> fetchTopOfTheWeekBooks() async {
+    try {
+      //check if there is an cached books
+      var cachedBooks = homeLocalDataSource.fetchTopOfTheWeekBooks();
+      if (cachedBooks.isNotEmpty) {
+        return right(cachedBooks);
+      }
+      var books = await homeRemoteDataSource.fetchTopOfTheWeekBooks();
+      return right(books);
+    } catch (e) {
+      return left(Failure(error: e.toString()));
+    }
+  }
+}
